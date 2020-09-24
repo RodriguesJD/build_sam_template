80a711c19f2dfc2fd8f8b5cfe484aececa36d614
Jeremy Rodrigues <jrodrigues@ripple.com>
2020-09-24 10:26:58 -0700

    Converted script to class.

--- build_sam_helper.py
+++ build_sam_helper.py
@@ -6,120 +6,130 @@ import pkgutil
 import pip._internal.operations.freeze
 
 
-project_name = input("Name of lambda project folder")
+class BuildSamTemplate:
+    project_name = input("Name of lambda project folder")
 
+    # TODO look for sam_vars.py for configuration vars
 
-def description():
-    des = input("what is the Description")
-    return des
+    @staticmethod
+    def description():
+        des = input("what is the Description")
+        return des
 
-
-dict_file = {'AWSTemplateFormatVersion': '2010-09-09',
-             'Transform': 'AWS::Serverless-2016-10-31',
-             'Description': description(),
-             'Globals': {
-                 'Function': {
-                     'Timeout': 3}},
-             'Resources': {
-                 f'{project_name.title().replace("_", "")}Function': {
-                     'Type': 'AWS::Serverless::Function',
-                     'Properties': {
-                         'CodeUri': f'{project_name}/',
-                         'Handler': 'app.lambda_handler',
-                         'Runtime': 'python3.7',
-                         'Events': {
-                             'HelloWorld': {
-                                 'Type': 'Schedule',
-                                 'Properties': {
-                                     'Schedule': 'rate(5 minutes)',
-                                     'Description': 'Example schedule',
-                                     'Name': f'{project_name.replace("_", "-")}-schedule',
-                                     'Enabled': True}
+    def template_dict(self):
+        dict_file = {'AWSTemplateFormatVersion': '2010-09-09',
+                     'Transform': 'AWS::Serverless-2016-10-31',
+                     'Description': self.description(),
+                     'Globals': {
+                         'Function': {
+                             'Timeout': 3}},
+                     'Resources': {
+                         f'{self.project_name.title().replace("_", "")}Function': {
+                             'Type': 'AWS::Serverless::Function',
+                             'Properties': {
+                                 'CodeUri': f'{self.project_name}/',
+                                 'Handler': 'app.lambda_handler',
+                                 'Runtime': 'python3.7',
+                                 'Events': {
+                                     'HelloWorld': {
+                                         'Type': 'Schedule',
+                                         'Properties': {
+                                             'Schedule': 'rate(5 minutes)',
+                                             'Description': 'Example schedule',
+                                             'Name': f'{self.project_name.replace("_", "-")}-schedule',
+                                             'Enabled': True}
+                                     }
+                                 }
                              }
                          }
                      }
-                 }
-             }
-             }
+                     }
+
+        with open('template.yaml', 'w') as file:
+            documents = yaml.dump(dict_file, file)
 
 
-with open('template.yaml', 'w') as file:
-    documents = yaml.dump(dict_file, file)
-
-
-if os.path.isdir(project_name):
-    pass
-else:
-    os.mkdir(project_name)
-
-if os.path.isdir(f"{project_name}/{project_name}"):
-    pass
-else:
-    os.mkdir(f"{project_name}/{project_name}")
-
-if os.path.isfile(f"{project_name}/__init__.py"):
-    pass
-else:
-    with open(f"{project_name}/__init__.py", 'w') as fp:
-        pass
-
-if os.path.isdir(f"{project_name}/{project_name}"):
-    pass
-else:
-    os.mkdir(f"{project_name}/{project_name}")
-
-if os.path.isfile(f"{project_name}/{project_name}/__init__.py"):
-    pass
-else:
-    with open(f"{project_name}/{project_name}/__init__.py", 'w') as fp:
-        pass
-
-with open(f"{project_name}/{project_name}/requirements.txt", 'w') as fp:
-    modules = pip._internal.operations.freeze.get_installed_distributions()
-    ignore_these_packages = ["wcwidth","six", "pytest", "pyparsing", "py", "pluggy", "packaging", "more-itertools",
-                             "attrs", "setuptools", "pip"]
-    for module in modules:
-        if module.key in ignore_these_packages:
+        if os.path.isdir(self.project_name):
             pass
         else:
-            fp.write(f"{module.key}\n")
+            os.mkdir(self.project_name)
 
-with open(f"{project_name}/{project_name}/app.py", 'w') as fp:
-    text = '''import json
+        if os.path.isdir(f"{self.project_name}/{self.project_name}"):
+            pass
+        else:
+            os.mkdir(f"{self.project_name}/{self.project_name}")
+
+        if os.path.isfile(f"{self.project_name}/__init__.py"):
+            pass
+        else:
+            with open(f"{self.project_name}/__init__.py", 'w') as fp:
+                pass
+
+        if os.path.isdir(f"{self.project_name}/{self.project_name}"):
+            pass
+        else:
+            os.mkdir(f"{self.project_name}/{self.project_name}")
+
+        if os.path.isfile(f"{self.project_name}/{self.project_name}/__init__.py"):
+            pass
+        else:
+            with open(f"{self.project_name}/{self.project_name}/__init__.py", 'w') as fp:
+                pass
+
+        with open(f"{self.project_name}/{self.project_name}/requirements.txt", 'w') as fp:
+            # TODO try using .toml file to extract this data
+            modules = pip._internal.operations.freeze.get_installed_distributions()
+            ignore_these_packages = ["wcwidth","six", "pytest", "pyparsing", "py", "pluggy", "packaging", "more-itertools",
+                                     "attrs", "setuptools", "pip"]
+            for module in modules:
+                if module.key in ignore_these_packages:
+                    pass
+                else:
+                    fp.write(f"{module.key}\n")
+
+        with open(f"{self.project_name}/{self.project_name}/app.py", 'w') as fp:
+            text = '''import json
+        
+        
+        def lambda_handler(event, context):
+            """Sample pure Lambda function
+        
+            Parameters
+            ----------
+            event: dict, required
+                API Gateway Lambda Proxy Input Format
+        
+                Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+        
+            context: object, required
+                Lambda Context runtime methods and attributes
+        
+                Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+        
+            Returns
+            ------
+            API Gateway Lambda Proxy Output Format: dict
+        
+                Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+            """
+        
+            return {
+                "statusCode": 200,
+                "body": json.dumps({
+                    "message": "hello world",
+                }),
+            }
+        '''
+            fp.writelines(text)
+
+        shutil.copyfile('template.yaml', f"{self.project_name}/template.yaml")
+
+        print(f"mkdir {self.project_name}")
+        print(f"cp app.py {self.project_name}/app.py")
+
+    def create(self):
+        self.template_dict()
 
 
-def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-        }),
-    }
-'''
-    fp.writelines(text)
-
-shutil.copyfile('template.yaml', f"{project_name}/template.yaml")
-
-print(f"mkdir {project_name}")
-print(f"cp app.py {project_name}/app.py")
+if __name__ == '__main__':
+    BuildSamTemplate().create()
